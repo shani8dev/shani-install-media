ca3d05e166327808030a09bb57ce714d11755633
Shrinivas Kumbhar <shrinivas.v.kumbhar@gmail.com>
2025-02-23 00:56:39 +0530

    set localtime to UTC in build image

--- iso_profiles/airootfs/root/customize_airootfs.sh
+++ iso_profiles/airootfs/root/customize_airootfs.sh
@@ -6,14 +6,3 @@ echo "shani ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Enable greetd service.
 systemctl enable greetd
-
-# This ensures that the custom hook is applied after pacstrap has finished.
-if [ -f /root/archiso ]; then
-    echo "[INFO] Moving custom archiso hook from /root to /usr/lib/initcpio/hooks/archiso..."
-    mv /root/archiso /usr/lib/initcpio/hooks/archiso || {
-        echo "[ERROR] Failed to move custom archiso hook" >&2
-        exit 1
-    }
-else
-    echo "[WARNING] Custom archiso hook not found in /root; skipping move."
-fi
