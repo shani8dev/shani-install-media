c7565c98a974c6693923b79aa54f3c3fc66dde6f
Shrinivas Kumbhar <shrinivas.v.kumbhar@gmail.com>
2025-03-02 08:47:05 +0530

    use flatpak apps instead of repo apps

--- iso_profiles/profiledef.sh
+++ iso_profiles/profiledef.sh
@@ -12,7 +12,8 @@ bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '100%')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '100%')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-Xthreads' '0' '-Xultra' '-Xlong=31' '-b' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
