3ae35b5f99d69f00488d269ea9ef25290666b6d7
shrinivas kumbhar <shrinivas.kumbhar@verteil.com>
2026-01-14 01:56:39 +0530

    add cosmic profile

---
+++ iso_profiles/cosmic/profiledef.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="shanios-cosmic"
+iso_label="SHANI_$(date +%Y%m)"
+iso_publisher="Shani OS <https://shani.dev>"
+iso_application="Shani OS Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="shanios"
+buildmodes=('iso')
+bootmodes=('uefi.systemd-boot')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '100%')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers"]="0:0:400"
+  ["/root"]="0:0:750"
+)
